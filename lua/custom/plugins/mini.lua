563caba3b84b892f86c1c3fd0f64590b44e0b94d
Zerossive <47612836+Zerossive@users.noreply.github.com>
2024-04-24 17:12:22 -0500

    feat: adjust mini statusline

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -10,7 +10,34 @@ return { -- Collection of various small independent plugins/modules
 		-- [[ Mini Statusline ]]
 		local statusline = require 'mini.statusline'
 		-- set use_icons to true if you have a Nerd Font
-		statusline.setup { use_icons = vim.g.have_nerd_font }
+		statusline.setup {
+			use_icons = vim.g.have_nerd_font,
+			content = {
+				-- active = function() -- DEFAULT ACTIVE CONTENT
+				-- 	local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+				-- 	local git = MiniStatusline.section_git { trunc_width = 75 }
+				-- 	local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+				-- 	local filename = MiniStatusline.section_filename { trunc_width = 140 }
+				-- 	local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+				-- 	local location = MiniStatusline.section_location { trunc_width = 75 }
+				-- 	local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+				--
+				-- 	return MiniStatusline.combine_groups {
+				-- 		{ hl = mode_hl, strings = { mode } },
+				-- 		{ hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
+				-- 		'%<', -- Mark general truncate point
+				-- 		{ hl = 'MiniStatuslineFilename', strings = { filename } },
+				-- 		'%=', -- End left alignment
+				-- 		{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+				-- 		{ hl = mode_hl, strings = { search, location } },
+				-- 	}
+				-- end,
+				inactive = function()
+					-- return '%#MiniStatuslineInactive#%F%=' -- DEFAULT INACTIVE CONTENT
+					return '%#MiniStatuslineInactive#%F%m%r%h%w' -- added modified, readonly, help and preview flags
+				end,
+			},
+		}
 
 		-- You can configure sections in the statusline by overriding their
 		-- default behavior. For example, here we set the section for
